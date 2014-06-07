749c59ae2ce65ca071bd5f8bb78acd5fd8ae47a9
handstrings <pulling@handstrings.com>
2014-06-07 13:49:04 -0400

    Pull #28. Fixes #18.

--- README.md
+++ README.md
@@ -5,11 +5,8 @@ is an extension meant to bring vim-like functionality to
 
 ![""](http://i.minus.com/ibodq0DYcbsoYp.png)
 
-Pretty much all of the Vim keymapping is from 
-[CodeMirror](http://codemirror.net)'s /keymap/vim.js, but I modified
-things very heavy-handedly. The two files will probably need to 
-be maintained in parallel until I can figure out a way to do this
-natively in Brackets.
+All of the Vim keymapping is from [CodeMirror](http://codemirror.net)'s 
+/keymap/vim.js included with Brackets.
 
 ## Installing Vimderbar
 0. In the Brackets menu bar, choose **File > Extension Manager...** 
@@ -18,11 +15,16 @@ natively in Brackets.
 0. Enable Vimderbar via the Brackets menu with **View > Enable Vimderbar**.
 
 ## Features
++ New: `:` command history per-project or with a common history based on 
+`vimderbar.commonHistory` true/false set in brackets preferences.
 + Basic vim keybindings (hjkl, yy, dd, p, P, o, O, gg, G, etc.).
 + Search function (`/`) integrated with Brackets' native search.
 + Very basic `:` commands. `:w` saves, `:q` closes the document or the inline 
 editor in focus, and `:e` opens the "Navigate > Quick Open" menu.
-+ Inline editor support. Double-tap `Esc` to close an inline editor. 
++ Use `:clearHistory` to reset current history.
++ `:bp` and `:bn` implemented for open files, but Document order is different
+than what is shown in Working Files.
++ Inline editor support. `Esc` to close an inline editor in Normal mode.
 + Modes: Normal, Insert, Visual (Visual-line, too).
 + Vim bar displays command presses in Normal & Visual modes so the user knows what
 has been pressed.
@@ -31,7 +33,6 @@ has been pressed.
 ## Limitations
 + `:` commands are sloppy, only one can be performed at a single time.
 + Can't yet perform complex `:` commands (like `:1,8d` or even `:wq`).
-+ `:` does not yet have "history" (e.g. pressing Up to get previous commands).
 + Reverse search (`?`) has been disabled to accommodate the native 
 search. (Please open an issue if you want this to be restored.)
 
@@ -40,9 +41,6 @@ Please do, by all means, hack on this extension. For coding conventions,
 review the 
 [Brackets Coding Conventions]
 (https://github.com/adobe/brackets/wiki/Brackets%20Coding%20Conventions).
-
-__Note:__ The Vim.js file isn't my own code, so I have tried to adhere to 
-CodeMirror's style.
 
 Thanks!
 
---
+++ src/ExCommandHistory.js
@@ -0,0 +1,127 @@
+/* jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/* global define, localStorage, $, brackets */
+
+// holds vim ex commmand history in projectFile
+define(function (require, exports) {
+    var ProjectManager     = brackets.getModule("project/ProjectManager"),
+        PreferencesManager = brackets.getModule("preferences/PreferencesManager"),
+        vimderbarPrefs     = PreferencesManager.getExtensionPrefs("vimderbar"),
+        commandHistory,
+        historyPosition,
+        currentProject,
+        useCommonVimHistory;
+    
+    // Housekeeping
+    /**
+     * Clear commandHistory.
+     */
+    function resetHistory() {
+        commandHistory = [];
+        historyPosition = -1;
+        _persistHistory();
+    }
+    /**
+     * @private
+     * Switch history to another project.
+     * @param {String} project Project full path, used as lookup key in localStorage.
+     */
+    function _changeProject(project) {
+        if (useCommonVimHistory) {
+            currentProject = "common";
+        } else {
+            currentProject = project;
+        }
+        _fetchHistory();
+    }
+    // Persistence
+    /**
+     * @private
+     * Store current history in localStorage.
+     */
+    function _persistHistory() {
+        localStorage.setItem("fontface.vimderbar.history." + currentProject, JSON.stringify(commandHistory));
+    }
+    /**
+     * @private
+     * Retrieve current history from localStorage.
+     */
+    function _fetchHistory() {
+        historyPosition = -1;
+        var history = localStorage.getItem("fontface.vimderbar.history." + currentProject);
+        if (history) {
+            try {
+                commandHistory = JSON.parse(history);
+            } catch (e) {
+                console.error("vimderbar: history parse error", e);
+                commandHistory = [];
+            }
+        } else {
+            resetHistory();
+        }
+    }
+    // Status Bar Interaction
+    /**
+     * Add an ExCommand to history (prepend), reset history position, store history.
+     * @param {String} command ExCommand string to store in history.
+     */
+    function add(command) {
+        var index = $.inArray(command, commandHistory);
+        if (index >= 0) {
+            commandHistory.splice(index, 1);
+        }
+        commandHistory.unshift(command);
+        historyPosition = -1;
+        _persistHistory();
+    }
+    /**
+     * Get the previous ExCommand in history, move position index.
+     * @returns {String} ExCommand at new history position.
+     */
+    function getPrevHistoryItem() {
+        historyPosition++;
+        if (historyPosition >= commandHistory.length) {
+            historyPosition = commandHistory.length - 1;
+        }
+        return commandHistory[historyPosition];
+    }
+    /**
+     * Get the next ExCommand in history, move position index.
+     * @returns {String} ExCommand at new history position.
+     */
+    function getNextHistoryItem() {
+        historyPosition--;
+        if (historyPosition < 0) {
+            historyPosition = 0;
+        }
+        return commandHistory[historyPosition];
+    }
+    /**
+     * Reset history position (triggered on input area blur)
+     */
+    function exitHistory() {
+        historyPosition = -1;
+    }
+    
+    /**
+     * Get history preferences and change project history to current project.
+     */
+    function init() {
+        useCommonVimHistory = vimderbarPrefs.get("commonHistory");
+        _changeProject(ProjectManager.getProjectRoot().fullPath);
+        
+        $(ProjectManager).on("projectOpen", function () {
+            _changeProject(ProjectManager.getProjectRoot().fullPath);
+        });
+        vimderbarPrefs.on("change", function () {
+            useCommonVimHistory = vimderbarPrefs.get("commonHistory");
+            _changeProject(ProjectManager.getProjectRoot().fullPath);
+        });
+    }
+
+    exports.init = init;
+    exports.add = add;
+    exports.getPrevHistoryItem = getPrevHistoryItem;
+    exports.getNextHistoryItem = getNextHistoryItem;
+    exports.resetHistory = resetHistory;
+    exports.exitHistory = exitHistory;
+});
